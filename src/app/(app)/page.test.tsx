4649a2044dfcd9c6ff8d95801a61cfe3492bf68f
Danail <DanailMinchev@users.noreply.github.com>
2025-02-01 17:18:43 +0200

    feat(testing): add Vitest - basic setup

---
+++ src/app/(app)/page.test.tsx
@@ -0,0 +1,14 @@
+import { expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Page from "./page";
+
+test("Page", () => {
+  render(<Page />);
+
+  expect(
+    screen.getByRole("heading", {
+      level: 1,
+      name: "Payload CMS and Clerk example",
+    }),
+  ).toBeDefined();
+});
